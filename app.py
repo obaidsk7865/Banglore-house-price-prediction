e4e04278f1f7bac01251cf9d55c5235e2c413c9d
obaidsk7865 <obaidsk7865@gmail.com>
2023-02-22 12:50:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import sklearn
+import numpy as np
+
+#import the model
+
+pipe = pickle.load(open('RidgeModel.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
+
+st.title("House Predicton")
+
+#brand
+location = st.selectbox("location",df['location'].unique())
+
+# type of laptop
+bhk = st.selectbox("bhk",df['bhk'].unique())
+
+#Ram
+bathrooms = st.selectbox("Bath",[1,2,3,4])
+
+# Weight
+squarefeet = st.number_input('Enter Square feet')
+
+if st.button('Predict Price'):
+    #query
+    input = pd.DataFrame([[location,squarefeet,bathrooms,bhk]],columns=['location','total_sqft','bath','bhk'])
+
+    # query = query.reshape(1,12)
+    st.title("The predicted price is " + str(int(pipe.predict(input)[0])) +'L')
